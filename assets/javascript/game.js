9541fd2cb3bf58e59626074e40f8f77b8ac9b5b0
Adam Norton <adam@adamnorton.com>
2019-09-17 13:26:28 -0500

    creating variables and capturing key press event.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 var alphabet = 'abcdefghijklmnopqrstuvwxyz';
-var guess = " ";
+var userGuess = " ";
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
@@ -9,6 +9,16 @@ var randomLetter = " ";
 function pickRandomLetter(){
     randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)]
 }
+
+document.onkeyup = function (keyPress) {
+
+    // Determines which key was pressed.
+    var userGuess = keyPress.key;
+    document.getElementById("userGuess").innerHTML = userGuess;
+
+    console.log(randomLetter);
+
+};
 
 function reset(){
     
