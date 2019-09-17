2f7b0e8b7a5e74c1d2eddcfb5ceda6eabd885f5d
Adam Norton <adam@adamnorton.com>
2019-09-17 15:33:00 -0500

    scripted initial values to appear in HTML page.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,28 @@ var alphabet = 'abcdefghijklmnopqrstuvwxyz';
 var userGuess = " ";
 var wins = 0;
 var losses = 0;
-var guessesLeft = 10;
+var guessesLeft = 9;
 var guessesSoFar = 0;
 var randomLetter = " ";
 
+resetGame();
+
+function resetGame(){
+    // reset initial values
+    guess = " ";
+    guessesLeft = 9;
+    guessesSoFar = 0;
+    userGuess = " ";
+    wins = 0;
+    losses = 0;
+
+    document.getElementById('guessesLeft').innerHTML = guessesLeft;
+    document.getElementById('guessesSoFar').innerHTML = guessesSoFar;
+}
+
 function pickRandomLetter(){
     randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)]
+    document.getElementById("letter").innerHTML = randomLetter;
 }
 
 document.onkeyup = function (keyPress) {
@@ -15,24 +31,8 @@ document.onkeyup = function (keyPress) {
     // Determines which key was pressed.
     var userGuess = keyPress.key;
     document.getElementById("userGuess").innerHTML = userGuess;
-
-    console.log(randomLetter);
-
 };
 
-function reset(){
-    
-    if(guess === randomLetter){
-        wins++;
-    } else {
-        losses++;
-    }
 
-    pickRandomLetter();
-    guess = " ";
-    guessesLeft = 10;
-    guessesSoFar = 0;
-}
 
 pickRandomLetter();
-console.log(randomLetter);
